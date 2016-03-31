c72877a3288bb12c220fe64b2e38bc45db92c4e1
benedekb <benedekbucsi@gmail.com>
2016-03-31 13:42:42 +0200

    Interfacek hozzáadva

---
+++ Nikpc/Nikpc/Interfaces/ITermekListaKezelo.cs
@@ -0,0 +1,12 @@
+﻿Ausing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nikpc.Interfaces
+{
+    interface ITermekListaKezelo
+    {
+    }
+}
