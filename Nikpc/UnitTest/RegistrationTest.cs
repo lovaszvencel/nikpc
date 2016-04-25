41729c13f8386cf823ec90bdaa3b89853aa7b261
Vencel Lovasz <v.lovasz@pharmindex.hu>
2016-04-25 19:16:26 +0200

    unit test 1

---
+++ Nikpc/UnitTest/RegistrationTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nikpc.Controllers;
+using Nikpc.Windows;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class RegistrationTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RegTestNullParameter()
+        {
+            UserController uc = new UserController();
+            uc.AddUser(null);
+        }
+    }
+}
