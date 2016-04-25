19991fca3548ddc9061d9d40528732e8a90bd040
benedekb <benedekbucsi@gmail.com>
2016-04-25 22:33:00 +0200

    Order admin

--- Nikpc/Nikpc/Windows/ModifyingOrderListWindow.xaml.cs
+++ Nikpc/Nikpc/Windows/ModifyingOrderListWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nikpc.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,34 @@ namespace Nikpc.Windows
     /// </summary>
     public partial class ModifyingOrderListWindow : Window
     {
+        OrderController oc;
+
         public ModifyingOrderListWindow()
         {
+            oc = new OrderController();
             InitializeComponent();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (orderList.SelectedItem != null)
+            {
+                if (MessageBox.Show("Biztosan törli?", "Figyelmeztetés", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+                {
+                    oc.DeleteOrder((Order)orderList.SelectedItem);
+                    MessageBox.Show("Sikeres törlés!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Jelöljön ki egy elemet!");
+            }
+
+        }
+
+        private void backButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
